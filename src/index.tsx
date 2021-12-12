d2fe4767972e5f562fbe29a0bd102b576710c858
Mohan Kumar Sagadevan <mohankumar27197@gmail.com>
2021-12-12 19:33:30 +0530

    added redux store functionality

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,12 @@
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
 import App from "./App";
+import { store } from "./state";
 
-ReactDOM.render(<App />, document.querySelector("#root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.querySelector("#root")
+);
